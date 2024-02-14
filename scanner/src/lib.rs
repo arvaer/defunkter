433c3b9734d098fa3aebf66dea5da07ce2a5f350
arvaer <mjalmeida172@gmail.com>
2024-02-14 02:32:17 +0100

    working through the chapter still

--- scanner/src/lib.rs
+++ scanner/src/lib.rs
@@ -1,4 +1,6 @@
+pub mod errors;
 pub mod tokens;
+use errors::*;
 use tokens::*;
 
 pub struct Scanner {
@@ -19,9 +21,16 @@ impl Scanner {
         }
     }
 
+    fn is_at_end(&self) -> bool {
+        self.current > self.source.len()
+    }
+
+    fn is_digit(&self, c: char) -> bool {
+        return c >= '0' && c <= '9';
+    }
+
     pub fn scan_tokens(&mut self) -> Vec<Token> {
-        let is_at_end = { self.current > self.source.len() };
-        while !is_at_end {
+        while self.is_at_end() {
             self.start = self.current;
             self.scan_token();
         }
@@ -32,6 +41,11 @@ impl Scanner {
 
     fn scan_token(&mut self) {
         let c: char = self.advance();
+        if self.is_digit(c) {
+            self.number();
+            return;
+        }
+
         match c {
             '(' => self.add_token(TokenType::LeftParen),
             ')' => self.add_token(TokenType::RightParen),
@@ -43,13 +57,84 @@ impl Scanner {
             '+' => self.add_token(TokenType::Plus),
             ';' => self.add_token(TokenType::Semicolon),
             '*' => self.add_token(TokenType::Star),
-            _ => todo!(),
+            '!' => {
+                if self.check_next('=') {
+                    self.add_token(TokenType::BangEqual);
+                } else {
+                    self.add_token(TokenType::Bang);
+                }
+            }
+            '=' => {
+                if self.check_next('=') {
+                    self.add_token(TokenType::EqualEqual);
+                } else {
+                    self.add_token(TokenType::Equal);
+                }
+            }
+            '<' => {
+                if self.check_next('=') {
+                    self.add_token(TokenType::LessEqual);
+                } else {
+                    self.add_token(TokenType::Less);
+                }
+            }
+            '>' => {
+                if self.check_next('=') {
+                    self.add_token(TokenType::GreaterEqual);
+                } else {
+                    self.add_token(TokenType::Greater);
+                }
+            }
+            '/' => {
+                if self.check_next('/') {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        let _ = self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::Slash)
+                }
+            }
+            ' ' | '\r' | '\t' => (),
+            '\n' => self.line += 1,
+            '"' => self.string(),
+            _ => {
+                if !self.is_digit(c) {
+                    let _ = (self.line, "Unexpected character.");
+                }
+            } ,
         }
     }
 
     fn add_token(&mut self, token_type: TokenType) {
         self.add_token_literal(token_type, None);
     }
+
+    fn check_next(&mut self, expected: char) -> bool {
+        if self.current >= self.source.len() {
+            return false;
+        }
+        if self.source.chars().nth(self.current).unwrap() != expected {
+            return false;
+        }
+        self.current += 1;
+        return true;
+    }
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            return '\0';
+        }
+        self.source.chars().nth(self.current).unwrap()
+    }
+    fn peek_next(&self) -> char {
+        let check_end = {
+            self.current + 1 >= self.source.len()
+        };
+        if check_end {
+            return '\0'
+        }
+        return self.source.chars().nth(self.current + 1).unwrap();
+    }
+
     fn add_token_literal(&mut self, token_type: TokenType, literal: Option<String>) {
         let text = self
             .source
@@ -63,8 +148,42 @@ impl Scanner {
     }
 
     fn advance(&mut self) -> char {
+        let c = self.source.chars().nth(self.current).unwrap();
         self.current += 1;
-        return self.source.chars().nth(self.current).unwrap();
+        c
+    }
+
+    fn string(&mut self) {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1
+            }
+            self.advance();
+        }
+        if self.is_at_end() {
+            error(self.line, "Unterminated String");
+        }
+
+        self.advance();
+
+        let value: String = self
+            .source
+            .chars()
+            .skip(self.start + 1)
+            .take(self.current - 1)
+            .collect();
+        self.add_token_literal(TokenType::String, Some(value))
+    }
+
+    fn number(&mut self) {
+        while self.is_digit(self.peek()) {
+            self.advance();
+        }
+        if self.peek() == '.' && self.is_digit(self.peek_next()) {
+            self.advance();
+            while self.is_digit(self.peek()) { self.advance(); }
+        }
+
     }
 }
 
@@ -84,6 +203,4 @@ mod tests {
         let tokens = scanner.scan_tokens();
         assert_eq!(tokens.len(), 4);
     }
-
-
 }
