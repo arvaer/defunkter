4b5bf991c0c466c8ecfc9ee84c1b02527dc02980
mikey <michael_almeida@cymantix.com>
2024-03-01 21:57:56 -0500

    working through the playbook

--- scanner/src/parser.rs
+++ scanner/src/parser.rs
@@ -6,57 +6,6 @@ use crate::{
 };
 use std::rc::Rc;
 
-pub enum Equality {
-    Left(Comparison),
-    Right(Vec<EqualitySubexp>),
-}
-pub enum EqualitySubexp {
-    Operator(Token),
-    Operand(Rc<Comparison>),
-}
-
-pub enum Comparison {
-    Left(Term),
-    Right(Vec<ComparisonSubexp>),
-}
-
-pub enum ComparisonSubexp {
-    Operator(Token),
-    Operands(Rc<Term>),
-}
-
-pub enum Term {
-    Left(Factor),
-    Right(Vec<TermSubexp>),
-}
-
-pub enum TermSubexp {
-    Operator(Token),
-    Operands(Rc<Factor>),
-}
-
-pub enum Factor {
-    Left(Rc<Unary>),
-    Right(Vec<FactorSubexp>),
-}
-
-pub enum FactorSubexp {
-    Operator(Token),
-    Operands(Rc<Unary>),
-}
-
-pub enum Unary {
-    Left { operation: Token, unary: Rc<Unary> },
-    Right(Primary),
-}
-
-pub enum Primary {
-    NUMBER(Token),
-    STRING(Token),
-    KEYWORD(Token),
-    GROUPING(Rc<Expression>),
-}
-
 pub struct Parser {
     tokens: Vec<Token>,
     current: usize,
@@ -76,23 +25,123 @@ impl Parser {
         Self { tokens, current: 0 }
     }
 
+    fn expression(&mut self) -> Option<Expression> {
+        return self.equality();
+    }
+    fn equality(&mut self) -> Option<Expression> {
+        let mut base_expr = self.comparison()?;
+        while let Some(token_type) = self.peek().map(|token| token.token_type.clone()) {
+            match token_type {
+                TokenType::BangEqual | TokenType::EqualEqual => {
+                    let operator = self.advance()?.clone();
+                    let right = self.comparison()?;
+
+                    base_expr = Expression::Binary {
+                        left: Rc::new(base_expr),
+                        operator,
+                        right: Rc::new(right),
+                    };
+                }
+                _ => break,
+            }
+        }
+        return Some(base_expr);
+    }
+
+    fn comparison(&mut self) -> Option<Expression> {
+        let mut base_expr = self.term()?;
+        while let Some(token_type) = self.peek().map(|token| token.token_type.clone()) {
+            match token_type {
+                TokenType::Greater
+                | TokenType::GreaterEqual
+                | TokenType::Less
+                | TokenType::LessEqual => {
+                    let operator = self.advance()?.clone();
+                    let right = self.term()?;
+
+                    base_expr = Expression::Binary {
+                        left: Rc::new(base_expr),
+                        operator,
+                        right: Rc::new(right),
+                    };
+                }
+                _ => break,
+            }
+        }
+        return Some(base_expr);
+    }
+
+    fn term(&mut self) -> Option<Expression> {
+        let mut base_expr = self.factor()?;
+        while let Some(token_type) = self.peek().map(|token| token.token_type.clone()) {
+            match token_type {
+                TokenType::Minus | TokenType::Plus => {
+                    let operator = self.advance()?.clone();
+                    let right = self.factor()?;
+
+                    base_expr = Expression::Binary {
+                        left: Rc::new(base_expr),
+                        operator,
+                        right: Rc::new(right),
+                    };
+                }
+                _ => break,
+            }
+        }
+        return Some(base_expr);
+    }
+
+    fn factor(&mut self) -> Option<Expression> {
+        let mut base_expr = self.unary()?;
+        while let Some(token_type) = self.peek().map(|token| token.token_type.clone()) {
+            match token_type {
+                TokenType::Slash | TokenType::Star => {
+                    let operator = self.advance()?.clone();
+                    let right = self.unary()?;
+
+                    base_expr = Expression::Binary {
+                        left: Rc::new(base_expr),
+                        operator,
+                        right: Rc::new(right),
+                    };
+                }
+                _ => break,
+            }
+        }
+        return Some(base_expr);
+    }
+
+    fn unary(&mut self) -> Option<Expression> {
+        let token_type = self.peek().map(|token| token.token_type.clone());
+        match token_type {
+            Some(TokenType::Bang) | Some(TokenType::Minus) => {
+                let operator = self.advance()?.clone();
+                let unary = self.unary()?;
+                return Some(Expression::Unary {
+                    operator,
+                    value: Rc::new(unary),
+                });
+            }
+            _ => {
+                return self.primary();
+            }
+        }
+    }
+
     fn primary(&mut self) -> Option<Expression> {
         let token_type = self.peek().map(|token| token.token_type.clone());
         match token_type {
             Some(TokenType::False) | Some(TokenType::True) | Some(TokenType::Nil) => {
-                self.advance();
-                let new_literal = Literal::KEYWORD(self.previous().unwrap().clone());
+                let new_literal = Literal::KEYWORD(self.advance().unwrap().clone());
                 return Some(Expression::Literal(new_literal));
             }
             Some(TokenType::String) => {
-                self.advance();
-                let string = self.previous().unwrap().clone();
+                let string = self.advance().unwrap().clone();
                 let new_literal = Literal::STRING(string);
                 return Some(Expression::Literal(new_literal));
             }
             Some(TokenType::Number) => {
-                self.advance();
-                let string = self.previous().unwrap().clone();
+                let string = self.advance().unwrap().clone();
                 let new_literal = Literal::NUMBER(string);
                 return Some(Expression::Literal(new_literal));
             }
@@ -102,16 +151,6 @@ impl Parser {
             Some(TokenType::Eof) | None | _ => {
                 return None;
             }
-        }
-    }
-
-    fn consume(&self, token: Token, message: &str) {
-        let token_type = self.peek().map(|token| token.token_type.clone());
-        match self.check(token_type) {
-            true => {
-                let _ = self.advance();
-            },
-            false => token_error(token, message)
         }
     }
 
